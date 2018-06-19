39798d2bb8b30512f0ad586fcc3f578abf8abc29
cassidytrinh <cassidytrinh@gmail.com>
2018-06-19 14:20:07 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,6 @@
 # write your method here
 
-def crazy_strings(a,b)
-  puts a.reverse.upcase + " " + b.swapcase.gsub("S", "Z")
+def crazy_strings(a, b)
+  toret = a.reverse.upcase + " " + b.swapcase.gsub("S", "Z")
+  puts toret
 end
-
-crazy_strings(a, b)
